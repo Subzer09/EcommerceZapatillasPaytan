3ea85ae0216a728c0cba6830be71e6580b5e58dd
Subzer09 <jimmypaytan10@gmail.com>
2022-07-06 21:32:41 -0500

    entregando primer desafio coderhouse

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  let nombreIdea = "Ecomerce Zapatillas Jimmy Paytan"
+  let nombreIdea = "Ecommerce Zapatillas Jimmy Paytan"
   return (
     <div className="App">
       <h1>{ nombreIdea }</h1>
