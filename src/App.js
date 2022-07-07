d66ce3420ce5a7d43635f200b251ecc0c58b3708
Subzer09 <jimmypaytan10@gmail.com>
2022-07-06 21:30:15 -0500

    entregando primer desafio

--- src/App.js
+++ src/App.js
@@ -2,22 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let nombreIdea = "Ecomerce Zapatillas Jimmy Paytan"
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{ nombreIdea }</h1>
     </div>
   );
 }
